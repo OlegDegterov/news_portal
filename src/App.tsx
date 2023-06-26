706768f49f795168a1492be7b99b507da3b67270
Дегтеров Олег Евгеньевич <degterovoe@sibur.ru>
2023-06-26 22:05:04 +0300

    theme scss variables useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Link, Route, Routes } from "react-router-dom"
 import { Counter } from "./components/Counter"
-import './index.scss'
+import './styles/index.scss'
 import { AboutAsyncPage } from "./pages/AboutPage/AboutAsyncPage"
 import { MainAsyncPage } from "./pages/MainPage/MainAsyncPage"
 import { Suspense } from "react"
+import { useTheme } from "./theme/useTheme"
 
 
 export const App = () => {
+    const {theme, toogleTheme} = useTheme()
 
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toogleTheme} >TOOGLE THEME</button>
             <Link to ={''}>Main Page</Link>
             <Link to ={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
